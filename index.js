0a4984f1f536e3a65fd92f65e10bee5323411a34
imane omari <imane3.omari@gmail.com>
2023-07-24 00:01:25 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+// Implement returnFirstTwoDrivers function
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+// Implement returnLastTwoDrivers function
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+// Implement selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+// Implement createFareMultiplier function
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+
+  
+// Implement fareDoubler function
+function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+  }
+  
+  // Implement fareTripler function
+  function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+  }
+  
+
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
+  
